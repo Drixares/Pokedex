2afd804a747851f70dad08be0316e108d8f88a4c
Matteo <matteo.lorenzoq@gmail.com>
2024-02-03 14:02:14 +0100

    ADD | Ajout système de recherche.

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 const searchInput = document.querySelector("#search");
 const boxError = document.getElementById("box-error");
 const btnMore = document.getElementById("btnMore");
-const searchBtn = document.getElementById("searchLabel");
+const searchBtn = document.getElementById("searchBtn");
 const boxCards = document.getElementById("box-cards");
 const requestApi = "https://pokeapi.co/api/v2/";
 const btnFilterGen = document.querySelectorAll('.G-btn button')
@@ -45,56 +45,60 @@ async function createCards(array) {
       console.log(indexNextGen);
     }
   });
-  console.log("passé");
-
+  
   const base = filtersGen[document.querySelector(".active-btn").dataset.gen];
-
-  for (let index = base+compteur-20; index < base+compteur && index < filtersGen[indexNextGen]; index++) { 
-    // console.log(index);
-    const pokemonSoloData = await fetch(`${requestApi}pokemon/${array[index].name}`);
-    const pokemonSolo = await pokemonSoloData.json()
-
-    const pokeCard = document.createElement("a");
-    pokeCard.setAttribute("class", `card card-${pokemonSolo.types[0].type.name} visible`);
-
-    pokeCard.id = `${pokemonSolo.name}`;
-
-    pokeCard.href = `pokemon/pokestats.html?id=${pokemonSolo.id}&name=${pokemonSolo.name}`;
-
-    // Fonction qui crée un span pour chaque type
-    let spanType = "";
-    for (let i = 0; i < pokemonSolo.types.length; i++) {
-      spanType += `<span>${pokemonSolo.types[i].type.name}</span>\n`;
-    }
-
-    pokeCard.innerHTML = `
-    <div>
-        <div class="box-name-number">
-            <span class="pokemon-name">${pokemonSolo.name}</span>
-            <span class="pokemon-number">#${pokemonSolo.id}</span>
-        </div>
-
-        <div class="box-infos">
-            <div class="box-type">
-                ${spanType}
-            </div>
-
-            <div class="box-img">
-                <img src="${pokemonSolo.sprites.other["official-artwork"].front_default}" width="193">
-            </div>
-        </div>
-    </div> `;
-
-    boxCards.appendChild(pokeCard);
-  };
-
-  compteur = compteur + 20
+  
+  if (base+compteur-20 < filtersGen[indexNextGen]) {
+    console.log("Lancé");
+    for (let index = base+compteur-20; index < base+compteur && index < filtersGen[indexNextGen]; index++) { 
+      // console.log(index);
+      const pokemonSoloData = await fetch(`${requestApi}pokemon/${array[index].name}`);
+      const pokemonSolo = await pokemonSoloData.json()
+  
+      const pokeCard = document.createElement("a");
+      pokeCard.setAttribute("class", `card card-${pokemonSolo.types[0].type.name} visible`);
+  
+      pokeCard.id = `${pokemonSolo.name}`;
+  
+      pokeCard.href = `pokemon/pokestats.html?id=${pokemonSolo.id}&name=${pokemonSolo.name}`;
+  
+      // Fonction qui crée un span pour chaque type
+      let spanType = "";
+      for (let i = 0; i < pokemonSolo.types.length; i++) {
+        spanType += `<span>${pokemonSolo.types[i].type.name}</span>\n`;
+      }
+  
+      pokeCard.innerHTML = `
+      <div>
+          <div class="box-name-number">
+              <span class="pokemon-name">${pokemonSolo.name}</span>
+              <span class="pokemon-number">#${pokemonSolo.id}</span>
+          </div>
+  
+          <div class="box-infos">
+              <div class="box-type">
+                  ${spanType}
+              </div>
+  
+              <div class="box-img">
+                  <img src="${pokemonSolo.sprites.other["official-artwork"].front_default}" width="193" alt="Image Pokemon : ${pokemonSolo.name}">
+              </div>
+          </div>
+      </div> `;
+  
+      boxCards.appendChild(pokeCard);
+    };
+  
+    compteur = compteur + 20
+  }
+  console.log(compteur);
   
 }
 
 
 searchBtn.addEventListener('click', async () => {
   boxCards.innerHTML = '';
+  compteur = 20;
 
   if (searchInput.value) {
 
@@ -131,13 +135,15 @@ searchBtn.addEventListener('click', async () => {
                 </div>
     
                 <div class="box-img">
-                    <img src="${pokemonSolo.sprites.other["official-artwork"].front_default}" width="193">
+                    <img src="${pokemonSolo.sprites.other["official-artwork"].front_default}" width="193" alt="Image Pokemon : ${pokemonSolo.name}">
                 </div>
             </div>
         </div> `;
     
         boxCards.appendChild(pokeCard);
       })
+
+      btnMore.style.display = "none"
   
     } else {
         boxError.innerHTML = '<h2>No pokemon found</h2>';
